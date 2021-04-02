50b3bdc17a3ac17818afedb11ed74c32ffce704d
Antti <antti@antti.codes>
2021-04-02 19:43:21 +0300

    change building scripts

--- build.sh
+++
@@ -1,1 +0,0 @@
-pkg . --out-path ./build/
--- dev.sh
+++
@@ -1,5 +0,0 @@
-# DEV SCRIPT TO BUILD AND MOVE WILMA CLI TO BIN
-# MADE ONLY FOR ME, CHANGE PATHS FOR YOUR INSTALLATION
-./build.sh
-rm "../../bin/wilma.exe"
-mv "./build/wilmacli-win.exe" "../../bin/wilma.exe"
